705cdbc36e52bd2d9f174714b8bd13826f6f2b48
roboton <roberton@gmail.com>
2022-08-04 01:12:37 -0700

    - add package metadata

--- R/scto_lib.R
+++ R/scto_lib.R
@@ -1,3 +1,10 @@
+# library(curl)
+# library(fs)
+# library(jsonlite)
+# library(lubridate)
+# library(readr)
+# library(stringr)
+
 #' Get an authenticated CSRF token
 #'
 #' Helper function to authenticate with SurveyCTO and fetch corresponding CSRF
@@ -6,15 +13,14 @@
 #' @param username character: username for the account used to login to the server.
 #' @param password character: password for the account used to login to the server.
 #' @return An authenticated CSRF token
-#' @examples
-#' csrf_token <- get_authenticated_csrf("my_server", "my_user", "my_password")
 get_authenticated_csrf <- function(servername, username, password) {
-  index_url <- str_glue("https://{servername}.surveycto.com/index.html")
-  index_res <- GET(index_url)
-  csrf_token <- headers(index_res)$`x-csrf-token`
+  index_url <- stringr::str_glue("https://{servername}.surveycto.com/index.html")
+  index_res <- httr::GET(index_url)
+  csrf_token <- httr::headers(index_res)$`x-csrf-token`
 
-  login_url <- str_glue("https://{servername}.surveycto.com/login?spring-security-redirect=%2F")
-  login_res <- POST(
+  login_url <- stringr::str_glue(
+    "https://{servername}.surveycto.com/login?spring-security-redirect=%2F")
+  login_res <- httr::POST(
     login_url,
     body = list(username = username,
                 password = password,
@@ -30,17 +36,15 @@ get_authenticated_csrf <- function(servername, username, password) {
 #' @param username character: username for the account used to login to the server.
 #' @param password character: password for the account used to login to the server.
 #' @return Authenticated SurveyCTO session boject
-#' @examples
-#' my_auth <- scto_auth("my_server", "my_user", "my_password")
 #' @export
 scto_auth <- function(servername, username, password) {
-  host <- str_glue("https://{servername}.surveycto.com")
+  host <- stringr::str_glue("https://{servername}.surveycto.com")
 
   curl_handle <- curl::new_handle()
   curl::handle_setopt(
-    handle = curl_handle,
-    httpauth = 1,
-    userpwd = stringr::str_glue("{username}:{password}"))
+      handle = curl_handle,
+      httpauth = 1,
+      userpwd = stringr::str_glue("{username}:{password}"))
 
   csrf_token <- get_authenticated_csrf(servername, username, password)
 
@@ -48,21 +52,21 @@ scto_auth <- function(servername, username, password) {
               servername = servername))
 }
 
-#' Access SurveyCTO data using the API
+#' Download SurveyCTO data using the API
 #'
 #' This function pulls data from SurveyCTO using their api.
-#' @param formid character: SurveyCTO Form ID.
-#' @param scto_auth an SurveyCTO authentication object from scto_auth function
-#' @param start_dt character: start datetime to begin fetching data (inclusive) [YYYY-MM-DD HH:MM:SS]
+#' @param dataset_id character: SurveyCTO Dataset ID.
+#' @param type character: "form" or "dataset" type
+#' @param scto_auth an SurveyCTO authentication object
+#' @param start_dt character: start datetime to begin fetching data (inclusive) YYYY-MM-DD HH:MM:SS
+#' @param refresh logical: whether or not to pull refreshed data or use local cache
+#' @param cache_dir character: path to store locally cached data
 #' @return A dataframe containing the data in wide format.
-#' @examples
-#' test_data <- scto_download("my_form", "my_server", "my_user", "my_password",
-#'                            "2020-01-01 12:00:00")
 #' @export
-scto_download <- function(id, type = "form", scto_auth, start_dt = 1,
-                          refresh = FALSE, cache_dir = "scto_data") {
+scto_pull <- function(dataset_id, type = "form", scto_auth, start_dt = 1,
+                          refresh = FALSE, cache_dir = NULL) {
+
   start_dt <- as.integer(lubridate::as_datetime(start_dt))
-  fs::dir_create(cache_dir, recurse = TRUE)
   local_file <- fs::path(
     cache_dir,
     stringr::str_glue("{scto_auth$servername}_{id}_{type}_{start_dt}.rds"))
@@ -83,6 +87,7 @@ scto_download <- function(id, type = "form", scto_auth, start_dt = 1,
   status <- response$status_code
   content <- rawToChar(response$content)
   if (status == 200) {
+    fs::dir_create(cache_dir, recurse = TRUE)
     if (type == "form") {
       scto_data <- as.data.frame(jsonlite::fromJSON(content, flatten = TRUE))
       readr::write_rds(scto_data, local_file)
@@ -103,15 +108,12 @@ scto_download <- function(id, type = "form", scto_auth, start_dt = 1,
 #' This function uploads a csv file to SurveyCTO using web POSTs and GETs to
 #' replace data in an existing Server Dataset
 #' @param data character: data frame to upload
-#' @param dataset_id character: dataset ID of existing Server Dataset to update
-#' @param dataset_title character: start datetime to begin fetching data (inclusive) [YYYY-MM-DD HH:MM:SS]
+#' @param dataset_id character: dataset ID of existing SCTO Server Dataset
+#' @param dataset_title character: dataset title of existing SCTO Server Dataset
 #' @param scto_auth an SurveyCTO authentication object from scto_auth function
 #' @return A dataframe containing the data in wide format.
-#' @examples
-#' upload_csv("my_server", "my_user", "my_password", "path_to_csv.csv",
-#'            "my_dataset", "My Dataset")
 #' @export
-scto_upload <- function(data, dataset_id, dataset_title, scto_auth) {
+scto_push <- function(data, dataset_id, dataset_title, scto_auth) {
   # TODO: potential function arguments that need to be tested/validated before
   # turning into actual function arguments.
   dataset_exists <- TRUE # possible to upload to non-existant datasets?
@@ -119,15 +121,15 @@ scto_upload <- function(data, dataset_id, dataset_title, scto_auth) {
   dataset_type <- "SERVER" # form dataset updates/uploads?
 
   # authentication
-  upload_url <- str_glue(
+  upload_url <- stringr::str_glue(
     "{scto_auth$host}/datasets/{dataset_id}/upload?csrf_token={scto_auth$csrf_token}")
 
   # write temporary csv file
   csv_file <- fs::file_temp(ext = "csv")
-  write_csv(data, csv_file)
+  readr::write_csv(data, csv_file)
 
   # data upload
-  upload_res <- POST(
+  upload_res <- httr::POST(
     upload_url,
     body = list(
       `dataset_exists` = as.numeric(dataset_exists),
@@ -135,7 +137,7 @@ scto_upload <- function(data, dataset_id, dataset_title, scto_auth) {
       `dataset_title` = dataset_title,
       `dataset_upload_mode` = dataset_upload_mode,
       `dataset_type` = dataset_type,
-      `dataset_file` = upload_file(csv_file)))
+      `dataset_file` = httr::upload_file(csv_file)))
 
   return(upload_res)
 }
